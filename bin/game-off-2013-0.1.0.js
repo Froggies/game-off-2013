5d826e782bab716ccb34d232993345ee1d4dfe1f
manland <rmaneschi@gmail.com>
2013-11-03 18:41:49 +0100

    end deploy bin

--- bin/game-off-2013-0.1.0.js
+++
@@ -1,8 +0,0 @@
-/**
- * game-off-2013 - v0.1.0 - 2013-11-03
- * 
- *
- * Copyright (c) 2013 Froggies Team
- * Licensed  <>
- */
-var game=function(){function a(){c()}function b(){window.clearInterval(d),d=void 0}function c(){d||(d=window.setInterval(function(){f+=1},e))}var d,e=50,f=0;return{start:function(){a()},pause:function(){b()},resume:function(){c()},getFps:function(){return e},getNbLoop:function(){return f},getBacklog:function(){return{}}}}();
